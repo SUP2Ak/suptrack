5339e221b314c8394dfe5aa2890363f84cab27d4
SUP2Ak <cormier.wesley@gmail.com>
2025-02-15 18:22:39 +0100

    ✨ feat(main): add support for regular and brand icons
    - include regular and brand FontAwesome icons in the library
    - enhance the icon options available for the application

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { MantineProvider, createTheme } from '@mantine/core';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import App from './App.tsx';
 import '@mantine/core/styles.css';
 import './index.css';
 
-library.add(fas);
+library.add(fas, far, fab);
 
 const theme = createTheme({
   fontFamily: 'Montserrat, sans-serif',
